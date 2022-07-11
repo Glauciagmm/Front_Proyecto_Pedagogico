8cf5d8ace26970daa9d7afc3444d6e0e1ac880a6
Yo123s <88155167+Yo123s@users.noreply.github.com>
2022-07-11 20:27:18 +0200

    Revert "Feature/assitant/listing"

--- proyecto-pedagogico/src/app/app.module.ts
+++ proyecto-pedagogico/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
-  BrowserModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
