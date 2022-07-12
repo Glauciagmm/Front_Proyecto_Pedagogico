5982d231a54ac760492897ee8464524920daad36
Iris <iris.vilaseca1998@gmail.com>
2022-07-12 09:11:58 +0200

    routing added

--- proyecto-pedagogico/src/app/app.module.ts
+++ proyecto-pedagogico/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
