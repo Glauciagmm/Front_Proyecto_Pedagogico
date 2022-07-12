ab9e4921d22ace552e2648f67a740173eebb2d2d
aoliverasd <aoliverasdelso@gmail.com>
2022-07-12 15:28:58 +0200

    Updated header and footer. Pending to fix logo and footer icons

--- proyecto-pedagogico/src/app/app-routing.module.ts
+++ proyecto-pedagogico/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule,Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'footertry', component: FooterComponent}
+  { path: 'header', component: HeaderComponent},
+  { path: 'footer', component: FooterComponent}
 ]
 
 
