3a4712f32e50f687b2abe89695c5cbac70989ece
Yo231s <dawid.lempicki.1@gmail.com>
2022-07-12 19:57:18 +0200

    feature

--- proyecto-pedagogico/src/app/app.module.ts
+++ proyecto-pedagogico/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CardComponent } from './card/card.component';
+import { ClientComponent } from './client/client.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CardComponent,
+    ClientComponent
   ],
   imports: [
   BrowserModule
