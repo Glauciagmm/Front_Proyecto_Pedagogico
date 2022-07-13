bb6ee21ab39c5386185c5ceca128d37316ac302e
unknown <flaquere@yahoo.es>
2022-07-13 18:09:52 +0200

    npi

--- proyecto-pedagogico/src/app/app.module.ts
+++ proyecto-pedagogico/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
