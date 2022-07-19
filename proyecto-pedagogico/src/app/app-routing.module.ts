f24dffe633868b9f12a016cf6f8532585aeb8b80
Iris <iris.vilaseca1998@gmail.com>
2022-07-19 12:51:33 +0200

    error messages now in Spanish

--- proyecto-pedagogico/src/app/app-routing.module.ts
+++ proyecto-pedagogico/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   //{ path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent},
   { path: 'header', component: HeaderComponent},
-  { path: 'footer', component: FooterComponent}
+  { path: 'footer', component: FooterComponent},
+  { path: 'login', component: LoginComponent}
 ]
 
 
