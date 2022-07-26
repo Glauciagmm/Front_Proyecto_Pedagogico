7fc5df19ef17cf18dcb7c911589a557ffd334470
Iris <iris.vilaseca1998@gmail.com>
2022-07-26 11:39:19 +0200

    better logged in page

--- proyecto-pedagogico/src/app/app-routing.module.ts
+++ proyecto-pedagogico/src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule,Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 
