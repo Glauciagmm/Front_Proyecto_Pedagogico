d839b3f8cbe4f72fb988f3620de1f7237d6fbcf7
aoliverasd <aoliverasdelso@gmail.com>
2022-07-27 09:38:05 +0200

    fixed app-component and routing

--- proyecto-pedagogico/src/app/app-routing.module.ts
+++ proyecto-pedagogico/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  //{ path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent}
