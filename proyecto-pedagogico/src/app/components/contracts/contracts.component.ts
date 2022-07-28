418adbf6a8c9927e9da8b4662971942ec7d0263d
Iris <iris.vilaseca1998@gmail.com>
2022-07-28 12:23:11 +0200

    html and TS for contract created

--- proyecto-pedagogico/src/app/components/contracts/contracts.component.ts
+++ proyecto-pedagogico/src/app/components/contracts/contracts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Contracts } from 'src/app/model/contracts';
+import { ContractService } from 'src/app/services/contract.service';
 
 @Component({
   selector: 'app-contracts',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contracts.component.css']
 })
 export class ContractsComponent implements OnInit {
+  public contracts: Contracts[] = [];
 
-  constructor() { }
+  selectedContract?: Contracts;
+  onSelect(contracts: Contracts): void {
+  this.selectedContract = contracts;
+  }
+
+  constructor(public contractsService: ContractService, private router: Router) {}
+
 
   ngOnInit(): void {
+    this.getContracts();
+  }
+  getContracts(): void {
+    this.contractsService.getContracts().subscribe((resp: any) => {
+      this.contracts = resp;
+      console.log(this.contracts);
+    });
+  }
+
+  add(): void {
+    this.router.navigate(["/contract-add"]);
+  }
+
+  delete(id: number): void {
+    this.contractsService.deleteContract(id).subscribe(
+      () => {
+        this.getContracts();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
 }
