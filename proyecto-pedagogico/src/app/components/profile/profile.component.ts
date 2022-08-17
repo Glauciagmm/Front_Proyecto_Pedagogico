c3dcad2f35f572bff77c1ac9e23f410ef62f58c2
unknown <flaquere@yahoo.es>
2022-08-17 19:29:10 +0200

    to new branch

--- proyecto-pedagogico/src/app/components/profile/profile.component.ts
+++ proyecto-pedagogico/src/app/components/profile/profile.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TokenStorageService } from '../../services/token-storage.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
+  currentUser: any;
+  constructor(private token: TokenStorageService) { }
   ngOnInit(): void {
+    this.currentUser = this.token.getUser();
+    console.log(this.currentUser);
   }
-
 }
