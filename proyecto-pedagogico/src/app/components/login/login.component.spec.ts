880e7655c8ca52eaa4487a8aecef668354fe3c4d
Iris <iris.vilaseca1998@gmail.com>
2022-08-22 09:34:47 +0200

    tried testing

--- proyecto-pedagogico/src/app/components/login/login.component.spec.ts
+++ proyecto-pedagogico/src/app/components/login/login.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let mockAuthService: jasmine.SpyObj<AuthService>;
+let router: Router;
 
   beforeEach(async () => {
+    mockAuthService = jasmine.createSpyObj<AuthService>('AuthService', ['login']);
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports:[RouterTestingModule, FormsModule, ReactiveFormsModule],
+      providers: [{provide: AuthService, useClass: mockAuthService}],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+
     })
     .compileComponents();
 
@@ -20,4 +31,9 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it ('should create a form with two controls', ()=>{
+    expect(component.form.contains('username')).toBeTruthy();
+    expect(component.form.contains('password')).toBeTruthy();
+  });
+
 });
