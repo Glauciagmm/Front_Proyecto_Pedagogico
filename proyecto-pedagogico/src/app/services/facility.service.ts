fd1bd45e3f889b5160ba33acd23796b2d2737c3a
Yo231s <dawid.lempicki.1@gmail.com>
2022-08-22 15:16:59 +0200

    facility

--- proyecto-pedagogico/src/app/services/facility.service.ts
+++ proyecto-pedagogico/src/app/services/facility.service.ts
@@ -1,90 +1,28 @@
-import { Facility } from "../models/facility"; 
-
-import { Injectable } from "@angular/core";
-
-// Import HttpClient and add it to constructor
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-
-import { catchError, map, Observable, throwError } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+import { Facility } from '../models/facility';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root'
 })
-
 export class FacilityService {
-    private handleError(error: HttpErrorResponse): any {
-        if (error.error instanceof ErrorEvent) {
-        console.error("An error occurred:", error.error.message);
-        } else {
-        console.error(
-            `Backend returned code ${error.status}, ` + `body was: ${error.error}`
-        );
-        }
-        return throwError("Something bad happened; please try again later.");
-    }
+  empUrl = "/api/facility";
+  constructor(private http: HttpClient) { }
 
-    private extractData(res: Response): any {
-        const body = res;
-        return body || {};
-    }
+  getAllEmployees(): Observable<Facility[]> {
+    return this.http.get<Facility[]>(this.empUrl).pipe(
+      tap(Facility => console.log("facility: " + JSON.stringify(Facility))),
+      catchError(this.handleError<Facility[]>([]))
+    );
+  }
 
-    private apiServerUrl = environment.apiBaseUrl;
-
-    constructor(private http: HttpClient) {
-        console.log("Oferred Services");
-    }
-    //Busca la funcionalidad de encontar todos los servicios el backend 
-    getFacilities(): Observable<Facility[]> {
-        return this.http.get<Facility[]>(`${this.apiServerUrl}/api/facility/list`);
-    }
-
-    //Busca la funcionalidad de encontar un servico por su id en el backend 
-    // public getFacility(id: number): Observable<Facility> {
-    // return this.http.get<Facility>(
-    //     `${this.apiServerUrl}/api/facility/${id}`
-    // );
-    // }
-
-    //Busca la funcionalidad de encontar un servico por su id en el backend 
-    getFacility(id: number): Observable<any> {
-        const httpOptions = {
-            headers: new HttpHeaders({
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            })
-        }
-    return this.http
-        .get(`${this.apiServerUrl}/api/facility/${id}`)
-        .pipe((catchError(this.handleError)));
-    }     
-
-    //Busca la funcionalidad de actualisar un servicio en el backend 
-    updateFacility(facility: Facility): Observable<any> {
-    console.log(facility);
-    return this.http.put<Facility>(
-        `${this.apiServerUrl}/api/facility/edit`, facility
-        )
-        .pipe(catchError(this.handleError));
-    }
-
-    //Busca la funcionalidad de borrar un servicio en el backend 
-    deleteFacility(id: number): Observable<any> {
-    return this.http
-        .delete<Facility>(
-        `${this.apiServerUrl}/api/facility/delete/${id}`
-        )
-        .pipe(catchError(this.handleError));
-    }
-
-    //Busca la funcionalidad de añadir un nuevo servicio en el backend 
-    public addFacility(facility: any): Observable<any> {
-    console.log(facility)
-    return this.http
-        .post<Facility>(
-        `${this.apiServerUrl}/api/facility/create`,
-        facility
-        )
-        .pipe(catchError(this.handleError));
+  private handleError<T>(result = {} as T) {
+    return (error: HttpErrorResponse): Observable<T> => {
+      console.error(error);
+      return of(result);
     }
 }
+    };
