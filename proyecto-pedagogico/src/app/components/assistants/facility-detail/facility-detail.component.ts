c248f79eedb909c5282f517d074d5a0c279efa82
Glauciagmm <glauciamesquitagmm@hotmail.com>
2022-08-31 09:46:38 +0200

    vista cliente

--- proyecto-pedagogico/src/app/components/assistants/facility-detail/facility-detail.component.ts
+++ proyecto-pedagogico/src/app/components/assistants/facility-detail/facility-detail.component.ts
@@ -1,63 +1,79 @@
-import { FacilityService } from '../../../services/facility.service'; 
+import { FacilityService } from '../../../services/facility.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Facility } from '../../../models/facility';
 import { Location } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ContractService } from 'src/app/services/contract.service';
+import * as moment from 'moment';
+import { Contract } from 'src/app/models/contract';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-facility-detail',
   templateUrl: './facility-detail.component.html',
-  styleUrls: ['./facility-detail.component.css']
+  styleUrls: ['./facility-detail.component.css'],
 })
 export class FacilityDetailComponent implements OnInit {
+  totalPrice: number | any = 0;
 
+  currentUser: any;
+
+  selectedContract?: Contract;
+  name!: string;
   facility!: Facility;
-  facilityId: String="0";
+  facilityId: String = '0';
   //facilityId: any=null;
 
   selectedFacility?: Facility;
-  onSelect(facility: Facility): void{
+  onSelect(facility: Facility, contract: Contract): void {
     this.selectedFacility = facility;
+    this.selectedContract = contract;
   }
-  // @Input() contractData = {
-  //   id: 0,
-  //   start: "",
-  //   finish: "",
-  //   totalPrice: 0,
-  
-  //   facility: {
-  //     id: 0,
-  //     title: "",
-  //     description: "",
-  //     pricePerHour: 0,
-  //     assistant: {
-  //       id: 0,
-  //       photo: "",
-  //       name: "",
-  //       city: "",
-  //     }
-  //   }
-  // }
-    constructor(
+
+  @Input() finish: String | any = '';
+  @Input() start: String | any = '';
+
+  @Input() contractData = {
+    id: 0,
+    start: '',
+    finish: '',
+    totalPrice: 0,
+
+    facility: {
+      id: 0,
+      title: '',
+      description: '',
+      pricePerHour: 0,
+      assistant: {
+        id: 0,
+        photo: '',
+        name: '',
+        city: '',
+      },
+    },
+  };
+
+  constructor(
     public facilityService: FacilityService,
     private route: ActivatedRoute,
     private router: Router,
     private Location: Location,
-    private contractService: ContractService
+    private contractService: ContractService,
+    private token: TokenStorageService
   ) {}
 
-
-
   ngOnInit(): void {
-    this.facilityId=(this.Location.path().toString().replace("/facility-details?id=",""))
+    this.currentUser = this.token.getUser();
+    this.facilityId = this.Location.path()
+      .toString()
+      .replace('/facility-details?id=', '');
     this.getFacility(this.facilityId);
   }
 
-  getFacility(facilityId:any):void{
+  getFacility(facilityId: any): void {
     this.facilityService.getFacility(facilityId).subscribe({
-      next: (resp: Facility)=>{
+      next: (resp: Facility) => {
         this.facility = resp;
       },
       error: (error: HttpErrorResponse) => {
@@ -66,6 +82,32 @@ export class FacilityDetailComponent implements OnInit {
     });
   }
 
+  getTotalPrice() {
+    console.log('Onchange');
+
+    let start = moment(this.start);
+    let finish = moment(this.finish);
+
+    let duration = moment.duration(finish.diff(start));
+    let time = duration.asHours();
+    this.contractData.totalPrice =
+      time * this.contractData.facility.pricePerHour;
+    // console.log(this.totalPrice);
+    // console.log(this.inicio);
+
+    //return this.contractData.totalPrice;
+  }
+
+  sendContractRequest(): void {
+    this.contractService.addContract(this.contractData).subscribe(
+      (result) => {
+        this.router.navigate(['/facility-details/' + result._id]);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
   // addContract():void{
   //   this.contractService.addContract(this.contractData).subscribe((result)=>{
   //     this.router.navigate(["/facility-details/" + result._id]);
@@ -84,7 +126,7 @@ export class FacilityDetailComponent implements OnInit {
       },
       (error) => {
         console.log(error);
-      },
+      }
     );
   }
 
@@ -92,6 +134,4 @@ export class FacilityDetailComponent implements OnInit {
   //   this.contractData.finish
   //   this.contractData.start
   // }
-
- 
 }
--- proyecto-pedagogico/src/app/components/contracts/contract-add/contract-add.component.ts
+++ proyecto-pedagogico/src/app/components/contracts/contract-add/contract-add.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Contract } from '../../../models/contract';
 import { ContractService } from '../../../services/contract.service';
 import { TokenStorageService } from '../../../services/token-storage.service';
 import { interval, timer } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-contract-add',
@@ -11,46 +12,13 @@ import { interval, timer } from 'rxjs';
   styleUrls: ['./contract-add.component.css'],
 })
 export class ContractAddComponent implements OnInit {
-  // totalHours (s: number) {
-  //   let ms = s % 1000;
-  //   s = (s - ms) / 1000;
-  //   let secs = s % 60;
-  //   s = (s - secs) / 60;
-  //   let mins = s % 60;
-  //   s = (s - mins)/60
-  //   let hrs = s % 24;
-  //   s = (s -hrs)/24;
-  //   return 'Time left: '+ hrs + 'h, ' + mins + 'm, ';
-  // }
+  totalPrice: number | any = 0;  
+  @Input() finish: String | any = "";
+  @Input() start: String | any= "";
 
-  time: number | any;
-  hours: number | any;
-  days: number | any;
-
-
-  finish: Date | any = new Date(2022-12-22);
-  start: Date | any= new Date(2022-12-20);
-  
-  timeInterval = 'start/finish';
-
-  getTime(){
-    
-    this.time = Math.abs(this.finish - this.start)/365;
-    
-  }
-  //(this.finish - this.start)/360000; 
-  
-    // this.days = this.time/(1000 * 3600 * 24);
-
-    //  console.log(this.days);
-
-  //  var hours = Math.abs(this.start - this.finish) / 36e5;
   currentUser: any;
 
   selectedContract?: Contract;
-  onSelect(contract: Contract): void {
-    this.selectedContract = contract;
-  }
 
   @Input() contractData = {
     id: 0,
@@ -80,28 +48,28 @@ export class ContractAddComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.currentUser = this.token.getUser();
-
-   this.getTime();
-   console.log(this.time);
-   
-
+   this.currentUser = this.token.getUser();
   }
 
-    //  var hours = Math.abs(this.start - this.finish) / 36e5;
-    // const range = moment.range(timeInterval);
+  getTotalPrice(){
+    console.log("Onchange");
+    
+  
+    let start = moment(this.start);
+    let  finish = moment(this.finish); 
+    
+    let  duration = moment.duration(finish.diff(start));
+    let  time = duration.asHours();
+    this.contractData.totalPrice = time * this.contractData.facility.pricePerHour;   
+    // console.log(this.totalPrice);
+    // console.log(this.inicio);
+    
+    //return this.contractData.totalPrice;     
+  }
 
-  // createContract(): void {
-  //   this.contractService.addContract(this.contract).subscribe(
-  //     (result) => {
-  //       this.router.navigate(['/contract-details/' + result.id]);
-  //       console.log(result);
-  //     },
-  //     (err) => {
-  //       console.log(err);
-  //     }
-  //   );
-  // }
+  onSelect(contract: Contract): void {
+    this.selectedContract = contract;
+  }
 
   sendContractRequest(): void {
     this.contractService.addContract(this.contractData).subscribe(
